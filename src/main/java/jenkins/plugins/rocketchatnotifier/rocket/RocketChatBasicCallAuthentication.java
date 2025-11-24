73f3b83d5cb51514c3f3c25f44a37c50ca89e250
Filipe Roque <froque@premium-minds.com>
2025-11-24 17:20:55 +0000

    Bump unirest to v3

--- src/main/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatBasicCallAuthentication.java
+++ src/main/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatBasicCallAuthentication.java
@@ -6,7 +6,7 @@ import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import kong.unirest.UnirestException;
-import org.json.JSONObject;
+import kong.unirest.json.JSONObject;
 
 /**
  * Authentication using username/ password
