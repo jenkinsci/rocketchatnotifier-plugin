0a0656bc5ad2f01030b2f551b8efc0cda6229c45
Filipe Roque <froque@premium-minds.com>
2025-11-25 01:18:05 +0000

    Fix wrong import for assertEquals

--- src/test/java/jenkins/plugins/rocketchatnotifier/RocketChatNotifierWithJenkinsTest.java
+++ src/test/java/jenkins/plugins/rocketchatnotifier/RocketChatNotifierWithJenkinsTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-import static net.sf.ezmorph.test.ArrayAssertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
