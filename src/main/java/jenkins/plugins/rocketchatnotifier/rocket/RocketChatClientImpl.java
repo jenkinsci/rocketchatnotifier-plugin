961567b26005dec4208d85d8a1f2303b93bc0803
jblass <julien.blass@semsoft-corp.com>
2026-09-01 16:32:29 +0200

    Replacing imports of `org.apache.commons.lang` by imports of `org.apache.commons.lang3.StringUtils` due to recent Jenkins removal of the obsolete Apache Commons Lang 2 library from the core system.

--- src/main/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatClientImpl.java
+++ src/main/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatClientImpl.java
@@ -11,7 +11,7 @@ import kong.unirest.GetRequest;
 import kong.unirest.Unirest;
 import kong.unirest.UnirestException;
 import org.apache.commons.text.StringEscapeUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.security.cert.CertificateException;
